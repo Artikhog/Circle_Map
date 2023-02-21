daa434fbb49b107564a018bf92d21e3eb32588ca
Artyom Khogoev <artikhog@yandex/ru>
2023-02-21 20:15:57 +0300

    add png

--- map.js
+++ map.js
@@ -1,83 +1,17 @@
-function init() {
-    var circle_map_canvas = document.getElementById('circle_map');
+function init_map() {
+    var canvas = document.getElementById('map');
+    var stage = new createjs.Stage(canvas);
+    add_ticker(6000, stage);
 
-    var circle_stage = new createjs.Stage(document.getElementById('circle_map'));
-    add_ticker(6000, circle_stage);
-    // var loader = load_Image();
+    var map = new Map(4, stage, canvas.width / 2, 700);
+    map.draw_all();
 
-    var main_drone_circle = new Drone(2, 2, 'red');
-    var circle_map = new Map(main_drone_circle, circle_stage, circle_map_canvas.width / 2, 300);
-    circle_map.draw_all();
-    add_keyboard(main_drone_circle, circle_map);
-}
-
-class Drone {
-    constructor(x, y, color) {
-        this.x = x;
-        this.y = y;
-        this.color = color;
-        this.drone = new createjs.Shape();
-    }
-    draw(scale, x = scale * this.x, y = scale * this.y) {
-        this.drone.graphics.beginFill(this.color)
-            .drawCircle(x, y, scale * 0.2);
-    }
-    born(stage) {
-        stage.addChild(this.drone);
-    }
-}
-function add_keyboard(main_drone_circle, circle_map) {
-    window.onkeydown = function (e) {
-        switch (e.code) {
-            case 'KeyW':
-                main_drone_circle.y -= 0.1;
-                circle_map.draw_all();
-                break;
-            case 'KeyS':
-                main_drone_circle.y += 0.1;
-                circle_map.draw_all();
-                break;
-            case 'KeyA':
-                main_drone_circle.x -= 0.1;
-                circle_map.draw_all();
-                break;
-            case 'KeyD':
-                main_drone_circle.x += 0.1;
-                circle_map.draw_all();
-                break;
-            case 'KeyE':
-                circle_map.draw_all();
-                circle_map.spin('right');
-                break;
-            case 'KeyQ':
-                circle_map.draw_all();
-                circle_map.spin('left');
-                break;
-
-
-            case 'KeyI':
-                circle_map.drone1.drone.y += 1;
-                circle_map.draw();
-                break;
-            case 'KeyK':
-                circle_map.drone1.drone.y -= 1;
-                circle_map.draw();
-                break;
-            case 'KeyJ':
-                circle_map.drone1.drone.regX += 1;
-                circle_map.draw();
-                break;
-            case 'KeyL':
-                circle_map.drone1.drone.regX -= 1;
-                circle_map.draw();
-                break;
-        }
-    }
+    add_keyboard(map.drones[map.main_drone_number], map);
 }
 
 class Map {
-    constructor(main_drone, stage, center, size, angle = 0, meter_size = 11) {
-        this.main_drone = main_drone;
+    constructor(main_drone_number, stage, center, size, angle = 0, meter_size = 11) {
+        this.main_drone_number = main_drone_number;
         this.stage = stage;
         this.center = center;
         this.size = size;
@@ -87,12 +21,14 @@ class Map {
         this.map_container = new createjs.Container();
         this.map_container.x = center;
         this.map_container.y = center;
-        this.map_container.regX = main_drone.x * this.scale;
-        this.map_container.regY = main_drone.y * this.scale;
-        this.starts = [new Start_Place(5.5, 5.5, 'red')];
+        this.starts = [];
         this.starts.length = 8;
-        this.drones = [new Drone(5.5,5.5, 'red')]
+        this.drones = []
         this.drones.length = 8;
+        this.chargers = []
+        this.chargers.length = 2;
+        this.factories = [];
+        this.factories.length = 4;
         this.add_objects();
     }
     draw_all() {
@@ -105,48 +41,49 @@ class Map {
         this.update();
         this.rotate();
         var background = new createjs.Shape();
-        background.graphics.beginFill("green")
+        background.graphics.beginFill("black")
             .drawRect(0, 0, this.size, this.size);
         this.map_container.addChild(background);
 
         for (let i = 1; i < this.meter_size; i++) {
             var line = new createjs.Shape();
             line.graphics.beginStroke("white");
-            line.graphics.moveTo(this.scale*i, 0);
-            line.graphics.lineTo(this.scale*i, this.size);
-            line.graphics.moveTo(0, this.scale*i);
-            line.graphics.lineTo(this.size, this.scale*i);
+            line.graphics.moveTo(this.scale * i, 0);
+            line.graphics.lineTo(this.scale * i, this.size);
+            line.graphics.moveTo(0, this.scale * i);
+            line.graphics.lineTo(this.size, this.scale * i);
             this.map_container.addChild(line);
         }
-        this.map_container.cache(0, 0, 3000, 3000);
         this.stage.addChild(this.map_container);
         this.add_mask();
-        this.show_info();
     }
     add_objects() {
         for (let i = 0; i < 4; i++) {
             this.drones[i] = new Drone(5.5, 5.5, 'blue');
             this.drones[i+4] = new Drone(5.5, 5.5, 'red');
+            this.starts[i] = new Place(5.5, 5.5, 'blue', 'start');
+            this.starts[i+4] = new Place(5.5, 5.5, 'red', 'start');
+            this.factories[i] = new Place(5.5, 5.5, 'grey', 'factory', 1.6);
         }
-        for (let i = 0; i < 4; i++) {
-            this.starts[i] = new Start_Place(5.5, 5.5, 'blue');
-            this.starts[i+4] = new Start_Place(5.5, 5.5, 'red');
-        }
+        this.chargers[0] = new Place(5.5, 5.5,'green', 'charger', 0.6);
+        this.chargers[1] = new Place(5.5, 5.5, 'green', 'charger', 0.6);
     }
     draw_objects() {
+        this.chargers[0].draw(this.scale);
+        this.chargers[1].draw(this.scale);
+        this.map_container.addChild(this.chargers[0].place, this.chargers[0].bitmap);
+        this.map_container.addChild(this.chargers[1].place, this.chargers[1].bitmap);
+
         for (let i = 0; i < 8; i++) {
             this.starts[i].draw(this.scale);
-            this.map_container.addChild(this.starts[i].start);
-        }
-        for (let i = 0; i < 7; i++) {
             this.drones[i].draw(this.scale);
+            this.map_container.addChild(this.starts[i].place, this.starts[i].bitmap);
             this.map_container.addChild(this.drones[i].drone);
         }
-
-        this.map_container.updateCache();
-        this.main_drone.draw(this.scale, this.center, this.center);
-        this.stage.addChild(this.main_drone.drone);
-
+        for (let i = 0; i < 4; i++) {
+            this.factories[i].draw(this.scale);
+            this.map_container.addChild(this.factories[i].place, this.factories[i].bitmap);
+        }
     }
     spin(right_left) {
         switch (right_left) {
@@ -169,13 +106,12 @@ class Map {
     rotate() {
         createjs.Tween.get(this.map_container).to({
             rotation: this.angle
-        }, 1000);
+        }, 100);
         this.stage.addChild(new createjs.Text(`angle:${this.angle}`, "20px Arial", "#ff7700"))
-
     }
     update() {
-        this.map_container.regX = this.main_drone.x * this.scale;
-        this.map_container.regY = this.main_drone.y * this.scale;
+        this.map_container.regX = this.drones[this.main_drone_number].x * this.scale + this.drones[this.main_drone_number].drone.x;
+        this.map_container.regY = this.drones[this.main_drone_number].y * this.scale + this.drones[this.main_drone_number].drone.y;
     }
     show_info() {
         var x_info = new createjs.Text(`x:${Math.round(this.main_drone.x*10)/10}`, "20px Arial", "#ff7700");
@@ -214,40 +150,70 @@ class Map {
         this.stage.addChild(mask);
     }
 }
-
-class Start_Place {
-    constructor(x, y, color) {
+class Drone {
+    constructor(x, y, color, scale_koef = 0.2) {
         this.x = x;
         this.y = y;
         this.color = color;
-        this.start = new createjs.Shape();
+        this.scale_koeff = scale_koef;
+        this.drone = new createjs.Shape();
     }
-    draw(scale, x = scale * (this.x-0.4), y = scale * (this.y-0.4)) {
-        this.start.graphics.beginFill(this.color)
-            .drawRect(x, y, scale*0.8, scale*0.8);
-        this.start.graphics.beginFill('white')
-            .drawCircle(x + 0.4 * scale, y + 0.4 * scale, scale*0.3);
-    }
-    born(stage) {
-        stage.addChild(this.start);
+    draw(scale, x = scale * this.x, y = scale * this.y) {
+        this.drone.graphics.beginFill(this.color)
+            .drawCircle(x, y, scale * this.scale_koeff);
     }
 }
 
-function  add_ticker(framerate = 3000, stage) {
+class Place{
+    constructor(x, y, color, path, scale_koef = 0.8) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.scale_koef = scale_koef;
+        this.place = new createjs.Shape();
+        this.bitmap = new createjs.Bitmap(document.getElementById(path));
+    }
+    draw(scale, x = scale * (this.x-this.scale_koef/2), y = scale * (this.y-this.scale_koef/2)) {
+        this.place.graphics.beginFill(this.color)
+            .drawRect(x, y, scale * this.scale_koef, scale * this.scale_koef);
+        this.bitmap.x = this.x * scale + this.place.x - scale * this.scale_koef * 0.4;
+        this.bitmap.y = this.y * scale + this.place.y - scale * this.scale_koef * 0.4;
+        this.bitmap.scaleX = this.bitmap.scaleY = scale * this.scale_koef * 0.8 / 176;
+    }
+}
+
+function add_ticker(framerate = 3000, stage) {
     createjs.Ticker.timingMode = createjs.Ticker.RAF_SYNCHED;
     createjs.Ticker.framerate = framerate;
     createjs.Ticker.addEventListener("tick", stage);
 }
 
-// function load_Image() {
-//     let manifest = [{
-//         src: "start.png", id: "start"}, {
-//         src: "factory.png", id: "factory"}, {
-//         src: "charger.png", id: "charger"
-//     }]
-//
-//     let loader = new createjs.LoadQueue(false);
-//     loader.addEventListener("complete", handleComplete);
-//     loader.loadManifest(manifest, true);
-//     return loader
-// }
+function add_keyboard(main_drone_circle, map) {
+    window.onkeydown = function (e) {
+        switch (e.code) {
+            case 'KeyW':
+                main_drone_circle.drone.y -= 0.1 * map.scale;
+                map.draw_all();
+                break;
+            case 'KeyS':
+                main_drone_circle.drone.y += 0.1 * map.scale;
+                map.draw_all();
+                break;
+            case 'KeyA':
+                main_drone_circle.drone.x -= 0.1 * map.scale;
+                map.draw_all();
+                break;
+            case 'KeyD':
+                main_drone_circle.drone.x += 0.1 * map.scale;
+                map.draw_all();
+                break;
+            case 'KeyE':
+                map.draw_all();
+                map.spin('right');
+                break;
+            case 'KeyQ':
+                map.draw_all();
+                map.spin('left');
+                break;
+        }
+    }}
